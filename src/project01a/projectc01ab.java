73475286dbefaca60a5f90853f43ba34cfe1fe0b
ferhatm@gmail.com <ferhatmandz@gmail>
2021-09-11 05:01:57 -0400

    merge conflict

--- src/project01a/projectc01ab.java
+++ src/project01a/projectc01ab.java
@@ -2,7 +2,7 @@ package project01a;
 
 public class projectc01ab {
 	public static void main(String[] args) {
-		
+		System.out.println("here is merge conflict i created from local masater");
 		
 	}
 
